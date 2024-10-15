5889bc8b9bb58d2377d7205a11e7be07d52d9be1
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-10-15 14:17:23 +0200

    Nivell 1 Exercici 7

---
+++ Nivell1/Exercici7/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Object> list = new ArrayList<>();
+
+        Collections.addAll(list, "Hello", 434, "Goodbye", 5, 32, "Thesis");
+
+        List<Object> sortedList = list.stream()
+                .sorted((o1, o2 )-> String.valueOf(o2).length() - String.valueOf(o1).length())
+                .collect(Collectors.toList());
+
+        sortedList.forEach(System.out::println);
+    }
+}
