2faf9868bf1d81709c0bc3f75272e04f07b3ab24
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-10-15 13:14:01 +0200

    mNivell 1 Exercici 3

---
+++ Nivell1/Exercici3/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> months = new ArrayList<>();
+
+        Collections.addAll(months, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+        months.forEach(month -> System.out.println(month));
+    }
+}
