ab641419edbc1ab9e1f93f7b4ac0ee4bee74093b
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-10-16 12:58:13 +0200

    corrections

--- Nivell2/Exercici2/src/main/java/org/example/Main.java
+++ Nivell2/Exercici2/src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<Integer> numberList = new ArrayList<>();
+
+        numberList.add(1);
+
+        String result = listNumbersOddOrEven(1, 4, 5, 35, 65);
+
+    }
+
+    public static String listNumbersOddOrEven(List<int> numbers) {
 
     }
 }
